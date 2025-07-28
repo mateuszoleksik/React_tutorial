44cbec25924750cb14ab95df9c643fe91b6f72da
Mateusz Oleksik <oleksikmateusz@gmail.com>
2025-07-28 18:58:26 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { CORE_CONCEPTS } from './data.js'
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+
+function App() {
+
+  function handleClick() {
+
+  }
+
+  return (
+    <div>
+      <Header/>
+      <main>
+        <section id ="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onClick={handleClick}>Components</TabButton>
+            <TabButton onClick={handleClick}>JSX</TabButton>
+            <TabButton onClick={handleClick}>Props</TabButton>
+            <TabButton onClick={handleClick}>Components</TabButton>
+          </menu>
+        </section>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+
+export default App;
