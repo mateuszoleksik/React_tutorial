2369b7ea7dd93cd73826b3aac7ef5c83e7744382
Mateusz Oleksik <oleksikmateusz@gmail.com>
2025-07-29 18:57:29 +0200

    use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { CORE_CONCEPTS } from './data.js'
+import { useState } from 'react'
+import { CORE_CONCEPTS, EXAMPLES } from './data.js'
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState();
+    
+  function handleClick(selectedButton) {
+    setSelectedTopic(selectedButton)
+  }
 
-  function handleClick() {
+  let tabContent = (<p>Please select a topic.</p>)
 
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">  
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -16,26 +34,23 @@ function App() {
         <section id ="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onClick={handleClick}>Components</TabButton>
-            <TabButton onClick={handleClick}>JSX</TabButton>
-            <TabButton onClick={handleClick}>Props</TabButton>
-            <TabButton onClick={handleClick}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleClick('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleClick('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleClick('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleClick('state')}>State</TabButton>
           </menu>
+            {tabContent}
         </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
   );
 }
-
 
 export default App;
